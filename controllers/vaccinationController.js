a7046e39ebeb18d9943edafd831b9252b913662d
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-06-15 11:41:20 +0300

    Response to GET request

--- controllers/vaccinationController.js
+++ controllers/vaccinationController.js
@@ -1,9 +1,14 @@
-import VaccinationSchema from '../models/vaccinationSchema.js'
+import Vaccinations from '../models/vaccinationSchema.js'
 
 export const getAllVaccinations = async (req, res) => {
   try {
-    const vaccinations = await VaccinationSchema.find()    
-    res.status(200).json(vaccinations) // return "OK" status and and vaccinations as json object
+    const vaccinations = await Vaccinations.find()    
+    if(vaccinations) {
+      res.send(vaccinations)
+      res.status(200) // return "OK" status and and vaccinations as json object
+    } else {
+      throw new Error('Vaccinations not found')
+    }
   } catch (error) {
     res.status(404).json({ message: error.message }) 
     // return "not found" status and error message
