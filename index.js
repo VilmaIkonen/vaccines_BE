8acc15df7869eb3450fe1bf94f7c5c34bd284e6c
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-06-15 11:48:01 +0300

    Callback function name fixed, redundant comment deleted

--- index.js
+++ index.js
@@ -2,33 +2,34 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import vaccineOrderRoutes from './routes/vaccineOrderRoutes.js'
-import vaccinationRoutes from './routes/vaccinationRoutes.js'
+import vaccinationRouter from './routes/vaccinationRoutes.js'
+import vaccineOrderRouter from './routes/vaccineOrderRoutes.js'
 
 dotenv.config()
 
 const app = express()
 
-// Bodyparser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
-app.use('/api/vaccinesorders', vaccineOrderRoutes)
-app.use('/api/vaccinations', vaccinationRoutes)
+app.use('/api/vaccinations', vaccinationRouter)
+app.use('/api/vaccinesorders', vaccineOrderRouter)
+
+
+// This should be seen when go to actual deployed version:
+app.get('/', (req, res) => {
+  res.send('Greetings from Vaccinations and VaccineOrders API!')
+})
 
 const port = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-  res.send({ message: 'I did it!' })
-})
 
 // DB in MongoDB and connecting to it
 const CONNECTION_URL = process.env.MONGODB_URI
 
 mongoose.connect(CONNECTION_URL, { 
-  useNewUrlParser: true, useUnifiedTopology: true,
-  // useFindAndModify: false
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
 })
   .then(() => {
     console.log('Connected to MongoDB')
@@ -36,5 +37,4 @@ mongoose.connect(CONNECTION_URL, {
   })
   .catch((error) => console.log(error.message))
 
-// TODO: Below code for preventing console warnings: check if it is really needed
 mongoose.set('useFindAndModify', false)
