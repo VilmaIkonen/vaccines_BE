2b09c4166724b2f9c67303b5c135c0e4f1115bcc
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-06-16 10:12:24 +0300

    Redundant 'else' deleted

--- controllers/vaccinationController.js
+++ controllers/vaccinationController.js
@@ -2,13 +2,11 @@ import Vaccinations from '../models/vaccinationSchema.js'
 
 export const getAllVaccinations = async (req, res) => {
   try {
-    const vaccinations = await Vaccinations.find()    
+    const vaccinations = await Vaccinations.find()
     if(vaccinations) {
       res.send(vaccinations)
       res.status(200) // return "OK" status and and vaccinations as json object
-    } else {
-      throw new Error('Vaccinations not found')
-    }
+    } 
   } catch (error) {
     res.status(404).json({ message: error.message }) 
     // return "not found" status and error message
--- controllers/vaccineOrderController.js
+++ controllers/vaccineOrderController.js
@@ -2,13 +2,11 @@ import VaccineOrders from '../models/vaccineOrderSchema.js'
 
 export const getAllVaccineOrders = async (req, res) => {
   try {
-    const vaccineOrders = await VaccineOrders.find()    
+    const vaccineOrders = await VaccineOrders.find()
     if(vaccineOrders) {
       res.send(vaccineOrders)
       res.status(200) // return "OK" status and and vaccineOrders as json object
-    } else {
-      throw new Error ('Vaccine orders not found') // if no vaccine orders in db
-    }
+    } 
   } catch (error) {
     res.status(404).json({ message: error.message }) 
     // return "not found" status and error message
